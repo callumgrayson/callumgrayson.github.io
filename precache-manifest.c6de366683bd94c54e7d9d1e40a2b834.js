2d95d12e5d2f76d5370ef0aae56d2db4a80b2e89
callumgrayson <callum@musiio.com>
2019-08-29 23:21:13 +0800

    Updates

---
+++ precache-manifest.c6de366683bd94c54e7d9d1e40a2b834.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff860f9113b0b73cc7e0cf2304b6248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f959e7851a03bb642a37",
+    "url": "/static/css/main.7729f31c.chunk.css"
+  },
+  {
+    "revision": "7b1f4b21376cafcca9f7",
+    "url": "/static/js/2.d00220fc.chunk.js"
+  },
+  {
+    "revision": "f959e7851a03bb642a37",
+    "url": "/static/js/main.cb0d3c84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "42d98203dd4115f8196ccb757d540cbd",
+    "url": "/static/media/clicky.42d98203.png"
+  },
+  {
+    "revision": "b741456a8f1b23e56e77d873ca94086e",
+    "url": "/static/media/compare-yourself.b741456a.png"
+  },
+  {
+    "revision": "5470cb8099eeb0e1691a1e02345e6522",
+    "url": "/static/media/face-text.5470cb80.png"
+  },
+  {
+    "revision": "792c90fd252149ba854ee505c13903fa",
+    "url": "/static/media/gluey-jots.792c90fd.png"
+  },
+  {
+    "revision": "ce09ba680ce6f374705a7786b436e892",
+    "url": "/static/media/jammming.ce09ba68.png"
+  },
+  {
+    "revision": "555a928ada16fe182d87ab151fe9396f",
+    "url": "/static/media/make-24.555a928a.png"
+  },
+  {
+    "revision": "1f569e0c9c64aa27d465340a88cd13a3",
+    "url": "/static/media/not-quite-news.1f569e0c.png"
+  },
+  {
+    "revision": "698064875cf1ccf775587576f096bf07",
+    "url": "/static/media/ravenous.69806487.png"
+  },
+  {
+    "revision": "8a3fbe30cfd48fdd7c608fff82924eda",
+    "url": "/static/media/sampler.8a3fbe30.jpg"
+  },
+  {
+    "revision": "87ee3b412fb6a92a7e97ba46f900ea43",
+    "url": "/static/media/svg-clicker.87ee3b41.png"
+  },
+  {
+    "revision": "aa6c2b9c3a87f66dde89fd1cd2929d65",
+    "url": "/static/media/swapi.aa6c2b9c.png"
+  },
+  {
+    "revision": "dc6633adc7ff4814f4d033315baf0b2c",
+    "url": "/static/media/text-speech.dc6633ad.png"
+  }
+]);
