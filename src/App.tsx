6e2e308d64a11b97bae107861f707f94b7e006b0
callumgrayson <36386171+callumgrayson@users.noreply.github.com>
2021-12-09 16:34:17 +0100

    new page with router

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Callum Grayson portfolio page
+    </div>
+  );
+}
+
+export default App;
