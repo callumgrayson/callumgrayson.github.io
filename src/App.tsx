daa1c6a3f927ba52fb75805330545834c82dd8ac
callumgrayson <36386171+callumgrayson@users.noreply.github.com>
2021-12-09 17:06:43 +0100

    new deploy flow

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Callum Grayson portfolio page
+      Callum Grayson portfolio page <b>Updated</b>
     </div>
   );
 }
