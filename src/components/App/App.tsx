fe2717505f1f707d6935fa2debf7806905d0d064
Callum <ztlimp@gmail.com>
2022-04-09 18:12:02 +1200

    Removes React app and changes to simple html with link to callumgrayson.com

--- src/components/App/App.tsx
+++
@@ -1,7 +0,0 @@
-// import * as React from "react";
-import AppRoutes from "../Routing/AppRoutes";
-import "./App.css";
-
-export default function App() {
-  return <AppRoutes />;
-}
--- src/components/Headings/AppHeading.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AppHeading({ children }) {
-  return <h1>{children}</h1>;
-}
-
-export default AppHeading;
